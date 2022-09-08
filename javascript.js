680a66b5dbfc314fd21a7c234138cf9f2b140d26
Jon Moore <jonevanmoore@yahoo.com>
2022-09-08 11:07:00 -0700

    complete

--- javascript.js
+++ javascript.js
@@ -47,4 +47,20 @@ window.addEventListener('DOMContentLoaded', () => {
         slides2[slideIndex2 - 1].style.display = "block";
         setTimeout(showSlides2, 4000); // Change image every 4 seconds
     }
+
+    // SNACK
+    let slideIndex3 = 0;
+    showSlides3();
+
+    function showSlides3() {
+        let i;
+        let slides3 = document.getElementsByClassName("mySlides3");
+        for (i = 0; i < slides3.length; i++) {
+            slides3[i].style.display = "none";
+        }
+        slideIndex3++;
+        if (slideIndex3 > slides3.length) { slideIndex3 = 1 }
+        slides3[slideIndex3 - 1].style.display = "block";
+        setTimeout(showSlides3, 4000); // Change image every 4 seconds
+    }
 })
